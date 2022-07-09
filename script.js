709073608e6c98ef74ee280b9a06e694edb7b0e1
diego98343 <96963643+diego98343@users.noreply.github.com>
2022-07-09 12:00:19 -0400

    first-commit

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+
+
+
+
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { myFetch } = require("./scriptHelper");
 
+
+
+
+window.addEventListener("load", function() {
+ 
    let listedPlanets;
+
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
+       function pickPlanet(planets) {
+    }
+    
    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -16,13 +16,102 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-   
-}
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+let pilotname = document.forms[0];
+let copilotname= document.forms[0];
+let fuellevels = document.forms[0];
+let cargomass = document.forms[0];
+
+let display = document.getElementById("faultyItems");
+let colorchange= document.getElementById("launchStatus");
+let form =document.forms[0];
+let list = display.innerHTML= `<ol>
+                                  <li>${pilotname[0].value}<li>
+                                  <li>${copilotname[1].value}</li>
+                                  <li>${fuellevels[2].value}</li>
+                                  <li>${cargomass[3].value}</li>
+                               <ol>`;
+ 
+
+function validateInput(testInput) {
+
+    if ( pilotname[0].value===""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if (isNaN(pilotname[0].value)=== false ){
+        window.confirm(" You entered a number input in pilot name. Please try again");
+        display.style.visibility="hidden"
+        return null
+
+    } else if (copilotname[1].value===""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if (isNaN(copilotname[1].value)=== false ){
+        window.confirm(" You entered a number input in copilot name. Please try again");
+        display.style.visibility="hidden"
+        return null
+
+    } else if (fuellevels[2].value=== ""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if(isNaN(fuellevels[2].value)=== true) {
+        window.confirm(" You entered a letter input in the wrong spot. Please try again");
+        display.style.visibility="hidden"
+        return null ;
+
+    } else if(cargomass[3].value=== ""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty Please try again");
+        return null;
+
+    } else if (isNaN(cargomass[3].value)=== true){
+        window.confirm(" You entered a letter input in the wrong spot Please try again");
+        display.style.visibility="hidden"
+        return null ;
+
+    } else if ( fuellevels[2].value<100000){
+        window.confirm("Fuel level too LOW Fill tank");
+        display.style.visibility="hidden"
+        return null ;
+
+    } else if (cargomass[3].value>5000000){
+        window.confirm("Ship is too HEAVY!! Please unload");
+        display.style.visibility="hidden"
+        return null ;
+    }
+    
+    
+    
+    else {
+        colorchange.innerHTML=" READY TO LAUNCH"
+        colorchange.style.color="red"
+        display.style.visibility="visible"
+        return display.innerHTML= `<ol>
+                                     <li>${pilotname[0].value}<li>
+                                     <li>${copilotname[1].value}</li>
+                                     <li>${fuellevels[2].value}</li>
+                                     <li>${cargomass[3].value}</li>
+                                </ol> `;
+    }
+    
+};
+
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();  
+
+    validateInput(list);
    
-}
+  });
+
+
 
 async function myFetch() {
     let planetsReturned;
@@ -33,11 +122,17 @@ async function myFetch() {
     return planetsReturned;
 }
 
+
+
 function pickPlanet(planets) {
 }
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+
+
+
