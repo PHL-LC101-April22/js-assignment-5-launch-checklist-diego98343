625d88413842921803f34618388652e35b8037cb
diego98343 <96963643+diego98343@users.noreply.github.com>
2022-07-10 15:27:48 -0400

    commit3

--- script.js
+++ script.js
@@ -1,38 +1,159 @@
 
+let pilotname = document.forms[0];
+let copilotname= document.forms[0];
+let fuellevels = document.forms[0];
+let cargomass = document.forms[0];
 
-
-
-// Write your JavaScript code here!
-
+let planet= document.getElementById("missionTarget");
+let display = document.getElementById("faultyItems");
+let colorchange= document.getElementById("launchStatus");
+let form =document.forms[0];
+let list = display.innerHTML += `<ol> 
+                                  <li>${pilotname[0].value}<li>
+                                  <li>${copilotname[1].value}</li>
+                                  <li>${fuellevels[2].value}</li>
+                                  <li>${cargomass[3].value}</li>
+                               </ol>`;
  
 
+function validateInput(testInput) {
+
+    if ( pilotname[0].value===""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if (isNaN(pilotname[0].value)=== false ){
+        window.confirm(" You entered a number input in pilot name. Please try again");
+        display.style.visibility="hidden"
+        return null
+
+    } else if (copilotname[1].value===""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if (isNaN(copilotname[1].value)=== false ){
+        window.confirm(" You entered a number input in copilot name. Please try again");
+        display.style.visibility="hidden"
+        return null
+
+    } else if (fuellevels[2].value=== ""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty. Please try again");
+        return null
+
+    } else if(isNaN(fuellevels[2].value)=== true) {
+        window.confirm(" You entered a letter input in the wrong spot. Please try again");
+        display.style.visibility="hidden"
+        return null ;
+
+    } else if(cargomass[3].value=== ""){
+        display.style.visibility="hidden"
+        window.confirm(" input is empty Please try again");
+        return null;
+
+    } else if (isNaN(cargomass[3].value)=== true){
+        window.confirm(" You entered a letter input in the wrong spot Please try again");
+        display.style.visibility="hidden"
+        return null ;
+
+    } else if ( fuellevels[2].value<10000){
+        window.confirm("Fuel level too LOW Fill tank");
+        display.style.visibility="hidden"
+        colorchange.innerHTML=" SHUTTLE NOT READY TO LAUNCH"
+        colorchange.style.color="red"
+        return null ;
+
+    } else if (cargomass[3].value>50000){
+        window.confirm("Ship is too HEAVY!! Please unload");
+        display.style.visibility="hidden"
+        colorchange.innerHTML=" SHUTTLE NOT READY TO LAUNCH"
+        colorchange.style.color="red"
+        return null ;
+    }
+    
+    
+    
+    else {
+        colorchange.innerHTML=" READY TO LAUNCH"
+        colorchange.style.color="green"
+        display.style.visibility="visible"
+        return display.innerHTML= `<ol>
+                                     <li>${pilotname[0].value}<li>
+                                     <li>${copilotname[1].value}</li>
+                                     <li>${fuellevels[2].value}</li>
+                                     <li>${cargomass[3].value}</li>
+                                </ol> `;
+    }
+    
+};
 
 
 
+form.addEventListener("submit", function(e) {
+    e.preventDefault();  
+
+    validateInput(list);
+   
+  });
+
+  let url ='https://handlers.education.launchcode.org/static/planets.json'
 
 
+async function myFetch2(){
+
+  let planetsretured=await fetch(url);
+  let data= await planetsretured.json();
+   let {planets} =data;
+   console.log(planets)
+
+}
 
 
+  async function myFetch() {
+    let planetsReturned;
+    
+    planetsReturned = await fetch(url).then( function(response) {
+      response.json().then(function(json){
 
+       let planet = json[Math.floor(Math.random()*6)];
+      
+       console.log(planet.name)
+       console.log(planet.image);
+       console.log(planet.moons);
+       console.log(planet.star);
+       console.log(planet.distance);
+      
+       
+      
+      })
+    });
 
-
+}
 
 window.addEventListener("load", function() {
  
-   let listedPlanets;
+    myFetch()
 
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse=myFetch();
-   listedPlanetsResponse.then(function (result) {
-    listedPlanets= result;
-     console.log(listedPlanets);
-   }).then(function () {
-    console.log(listedPlanets);
-    function pickPlanet(listedPlanets) {
-     return listedPlanets;
-    }
-      console.log(pickPlanet(listedPlanets));
-   })
-   
-});
+    myFetch2()
+    
+ });
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
 
